bfc985bb70bd4b5f3e7b8d7781cd91754aef2ccd
Allen Baker <althebaker43@gmail.com>
2015-12-06 10:40:11 -0800

    Implemented main loop for solver

--- Main.cpp
+++ Main.cpp
@@ -2,7 +2,17 @@
 #include "Serpent.h"
 #include "Block.h"
 #include <iostream>
+#include <fstream>
 
+
+static void DumpDimensions(
+        std::ostream&   outputStream,
+        Serpent*        serpent
+        );
+
+static bool IsSerpentCube(
+        Serpent*    serpent
+        );
 
 int
 main(
@@ -15,29 +25,90 @@ main(
         return 1;
     }
 
+    std::cout << "Reading from " << argv[1] << std::endl;
+
     Serpent* serpent = Serpent::CreateFromPath(argv[1]);
 
-    size_t sizeX = 0;
-    size_t sizeY = 0;
-    size_t sizeZ = 0;
-    serpent->getDimensions(sizeX, sizeY, sizeZ);
-    std::cout
-        << "X:" << sizeX << ' '
-        << "Y:" << sizeY << ' '
-        << "Z:" << sizeZ << ' '
-        << std::endl;
+    std::ofstream dimStream("dim.csv");
 
-    if (serpent->check() == true)
+    size_t stepCount = 0;
+
+    std::cout << "Starting compression." << std::endl;
+
+    while (true)
     {
-        std::cout << "Check passed." << std::endl;
+        DumpDimensions(dimStream, serpent);
+
+        if (stepCount > 1000)
+        {
+            std::cout << "Number of steps exceeded 1000." << std::endl;
+            break;
+        }
+        stepCount++;
+
+        if (serpent->compress() == false)
+        {
+            std::cout << "Compression failed." << std::endl;
+            break;
+        }
+
+        if (IsSerpentCube(serpent) == true)
+        {
+            std::cout << "Cube achieved." << std::endl;
+            break;
+        }
     }
-    else
-    {
-        std::cout << "Check failed." << std::endl;
-    }
+
+    DumpDimensions(dimStream, serpent);
+
+    dimStream.close();
 
     delete serpent;
 
     return 0;
+}
+
+void
+DumpDimensions(
+        std::ostream&   outputStream,
+        Serpent*        serpent
+        )
+{
+    size_t sizeX = 0;
+    size_t sizeY = 0;
+    size_t sizeZ = 0;
+
+    serpent->getDimensions(sizeX, sizeY, sizeZ);
+
+    outputStream
+        << sizeX << ' '
+        << sizeY << ' '
+        << sizeZ << ' '
+        << std::endl;
+}
+
+bool
+IsSerpentCube(
+        Serpent*    serpent
+        )
+{
+    size_t sizeX = 0;
+    size_t sizeY = 0;
+    size_t sizeZ = 0;
+
+    serpent->getDimensions(sizeX, sizeY, sizeZ);
+
+    if(
+            (sizeX == 3) &&
+            (sizeY == 3) &&
+            (sizeZ == 3)
+      )
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
